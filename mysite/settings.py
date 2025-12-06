5e95cf4a716cf43e2e65bacb290ba479d6815ae3
Mohammadi <amohammadia0086@gmail.com>
2025-12-06 21:44:20 +0330

    added static directoriees and urls in settings
    + static root
    + static url
    + media url
    + media root

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+print("DEBUG: BASE_DIR is:", BASE_DIR)
+print("DEBUG: Template DIRS is:", os.path.join(BASE_DIR, 'templates'))
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -55,7 +57,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR/ "templates"],
+        "DIRS": [BASE_DIR /  'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -66,8 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
-WSGI_APPLICATION = "mysite.wsgi.application"
 
 
 # Database
@@ -116,7 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "statics"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
