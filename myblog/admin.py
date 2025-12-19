65b57b28792c4edd18b2a0690f0dd6c4f8d0ee94
Mohammadi <amohammadia0086@gmail.com>
2025-12-19 21:05:37 +0330

    Customize Django Admin and Model Representation
    + Registered models in admin.py for both apps
    + Implemented PostAdmin with list_display and search_fields
    + Added date_hierarchy and list_filter to Admin panel
    + Defined __str__ method for better object naming
    + Configured Meta class for default ordering
    + Refined Contact model and content fields

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from myblog.models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "published_date"
+    empty_value_display = "empty"
+    list_display = ("title" ,"counted_views", "status", "published_date")
+    list_filter = ("status",)
+    search_fields = ["title", "content"]
+admin.site.register(Post,  PostAdmin)
