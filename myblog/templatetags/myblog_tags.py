c3c7d55ecfd5ae15963a5de5053caefec89ce054
Mohammadi <amohammadia0086@gmail.com>
2026-01-02 15:38:39 +0330

    feat: add custom template tag and snippet filter
    + Implemented a custom Django template tag to retrieve published posts.
    + Added a snippet filter to truncate post content in templates.
    + Updated templates to use the new filter for content previews.

--- myblog/templatetags/myblog_tags.py
+++ myblog/templatetags/myblog_tags.py
@@ -3,9 +3,11 @@ from myblog.models import Post
 register = template.Library()
 
 
-
-
-
 @register.simple_tag(name='get_all_posts')
 def get_posts():
     return Post.objects.filter(status=1)
+
+
+@register.filter
+def snippet(value, args= 20):
+    return value[:args] + '...'
