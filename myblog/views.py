9786f66c4a660abd19e67a5211377fabec60b4bd
Mohammadi <amohammadia0086@gmail.com>
2026-01-03 22:39:51 +0330

    Add pagination to blog view
    + Implemented pagination in myblog_view using Django's Paginator
    + Display 3 posts per page
    + Fallback to page 1 on invalid or empty page numbers

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from myblog.models import Post
 from django.utils import timezone 
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 def myblog_view(request, cat_name=None, author_username=None):
     now = timezone.now()
@@ -9,6 +10,16 @@ def myblog_view(request, cat_name=None, author_username=None):
      posts = posts.filter(category__name = cat_name)
     if  author_username:
         posts = posts.filter(author__username = author_username)
+
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+         posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
     context = {'posts': posts} 
     return render(request, 'myblog/blog-home.html', context)
 
