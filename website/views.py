dbb50558328942c1c1dc206f0c726223f030fc28
Mohammadi <amohammadia0086@gmail.com>
2026-01-28 17:46:04 +0330

    Implement Contact form using Django ModelForm
    - Created ContactForm using Django ModelForm
    - Connected form to Contact model
    - Replaced basic Form with ModelForm in test view
    - Handled form validation and saving data to database
    - Completed course-related form implementation

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from website.models import Contact
-from website.forms import NamForm
+from website.forms import NamForm,ContactForm
 
 def index_view(request):
     return render(request, "website/index.html")
@@ -16,15 +16,12 @@ def contact_view(request):
 
 def test_view(request):
     if request.method == "POST":
-        form = NamForm(request.POST)
+        form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            subject = form.cleaned_data['subject']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            print(name, subject, email, message)
+            form.save()
+           
             return HttpResponse('done')
         else:
             return HttpResponse('not valid')
-    form = NamForm()
+    form = ContactForm()
     return render(request, "myblog/test.html", {'form':form})
