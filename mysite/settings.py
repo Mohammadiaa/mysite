9163bcbc4950924ffb2e9136ee56d014eed3453f
Mohammadi <amohammadia0086@gmail.com>
2026-01-29 21:12:08 +0330

    Add django.contrib.humanize and use naturaltime filter
    - Added 'django.contrib.humanize' to INSTALLED_APPS.
    - Loaded humanize in templates with {% load humanize %}.
    - Updated sidebar to display post published_date using the 'naturaltime' filter for more readable timestamps.

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "website.apps.WebsiteConfig",
-    "myblog"
+    "myblog",
+    'django.contrib.humanize'
+    
 ]
 
 MIDDLEWARE = [
